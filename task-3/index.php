9a0819a1960bf689753c02c394160ea3acfceaf0
JoyDmi <demasya13@gmail.com>
2022-04-05 18:38:09 +0200

    other

---
+++ task-3/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    include "site.php";
+    foreach ($adTable as $ad) {
+        echo "<tr>";
+        echo "<td>" . $ad[0] . "</td>";
+        echo "<td>" . $ad[1] . "</td>";
+        echo "<td>" . $ad[2] . "</td>";
+        echo "<td>" . $ad[3] . "</td>";
+        echo "</tr>";
+    }
+
+?>
